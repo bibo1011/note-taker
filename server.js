5ce24b976b0a30a4ac9af91fa20c55f3c6e5632d
Blue Abad <bibo1011@gmail.com>
2020-10-01 23:26:12 -0700

    add uniqid function

--- server.js
+++ server.js
@@ -19,27 +19,24 @@ const text = require('./db/db.json')
 // HTML routes
 // index works even if commented
 // app.get('*', (req, res) => {
-//     // res.send('index123')
 //     res.sendFile(path.join(__dirname, 'index.html'));
 // });
 app.get('/notes', (req, res) => {
-    // res.send('note123')
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // API routes
 app.get('/api/notes', (req, res) => {
-    // res.send('show me the note')
     res.json(text)
 })
 app.post('/api/notes', (req, res) => {
-    // res.send('show me the note')
     const newText = req.body;
-    console.log(uniqid('note'));
-    uniqid('note')
+    // console.log(uniqid('note'));
+    req.body.id = uniqid('note');
     text.push(newText);
     res.json(newText);
 })
 
+//
 
 
