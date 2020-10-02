6973ba87de30d4a2ab9cb7e8c4ce329388a909b3
Blue Abad <bibo1011@gmail.com>
2020-10-01 22:58:10 -0700

    create api route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path');
 const express = require('express');
+const uniqid = require('uniqid');
 const PORT = 3001
 
 const app = express();
@@ -13,14 +14,32 @@ app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
 });
 
+const text = require('./db/db.json')
+
+// HTML routes
 // index works even if commented
 // app.get('*', (req, res) => {
 //     // res.send('index123')
 //     res.sendFile(path.join(__dirname, 'index.html'));
 // });
-
 app.get('/notes', (req, res) => {
     // res.send('note123')
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+// API routes
+app.get('/api/notes', (req, res) => {
+    // res.send('show me the note')
+    res.json(text)
+})
+app.post('/api/notes', (req, res) => {
+    // res.send('show me the note')
+    const newText = req.body;
+    console.log(uniqid('note'));
+    uniqid('note')
+    text.push(newText);
+    res.json(newText);
+})
+
+
 
